101a067d00ee3609e4eaa25627fcc18f772390ed
mengning <mengning@ustc.edu.cn>
2020-05-10 00:14:02 +0800

    Update time-asm.c

--- src/systemcall/time-asm.c
+++ src/systemcall/time-asm.c
@@ -5,10 +5,10 @@ int main()
       time_t tt;
       struct tm *t;
       asm volatile(
+          "movl $0, %%edi\n\t"   //EDI寄存器用于传递参数
           "movl $0xc9,%%eax\n\t" //使用EAX传递系统调用号
-          "movl $0, %%edi\n\t"   //EDI
-          "syscall\n\t"       //触发系统调用
-          "movq %%rax,%0\n\t" //通过EAX寄存器返回系统调用值
+          "syscall\n\t"          //触发系统调用
+          "movq %%rax,%0\n\t"    //保存返回值
           : "=m"(tt));
       t = localtime(&tt);
       printf("time: %d/%d/%d %d:%d:%d\n",
