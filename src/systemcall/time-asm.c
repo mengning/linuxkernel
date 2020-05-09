8bd99af385a4bad5a3e48d107a1aaa04714e4044
mengning <mengning@ustc.edu.cn>
2020-05-09 23:56:23 +0800

    ctrl+shift+I in *.c

--- src/systemcall/time-asm.c
+++ src/systemcall/time-asm.c
@@ -5,19 +5,18 @@ int main()
       time_t tt;
       struct tm *t;
       asm volatile(
-          "movl $0xc9,%%eax\n\t"//使用%eax传递系统调用号
-	  "movl $0, %%edi\n\t"
-          "syscall\n\t" //触发系统调用
-          "movq %%rax,%0\n\t"//通过 EAX 寄存器返回系统调用值      
-          :"=m"(tt)
-      );
+          "movl $0xc9,%%eax\n\t" //使用EAX传递系统调用号
+          "movl $0, %%edi\n\t"   //EDI
+          "syscall\n\t"       //触发系统调用
+          "movq %%rax,%0\n\t" //通过EAX寄存器返回系统调用值
+          : "=m"(tt));
       t = localtime(&tt);
       printf("time: %d/%d/%d %d:%d:%d\n",
-            t->tm_year+1900,
-            t->tm_mon,
-            t->tm_mday,
-            t->tm_hour,
-            t->tm_min,
-            t->tm_sec);
+             t->tm_year + 1900,
+             t->tm_mon,
+             t->tm_mday,
+             t->tm_hour,
+             t->tm_min,
+             t->tm_sec);
       return 0;
 }
--- src/systemcall/time.c
+++ src/systemcall/time.c
@@ -7,11 +7,11 @@ int main()
     tt = time(NULL);
     t = localtime(&tt);
     printf("time: %d/%d/%d  %d:%d:%d\n",
-        t->tm_year+1900,
-        t->tm_mon,
-        t->tm_mday, 
-        t->tm_hour, 
-        t->tm_min, 
-        t->tm_sec); 
+           t->tm_year + 1900,
+           t->tm_mon,
+           t->tm_mday,
+           t->tm_hour,
+           t->tm_min,
+           t->tm_sec);
     return 0;
 }
